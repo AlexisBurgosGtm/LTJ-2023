14ee195283713660058f898e82e81518c3e87483
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2023-06-08 20:31:44 -0600

    inicial ltj

--- build/controllers/CustomerVars.js
+++ build/controllers/CustomerVars.js
@@ -1,12 +1,12 @@
-let GlobalToken = 'DIST_JV';
+let GlobalToken = 'LTJ-COBAN';
 let GlobalCodSucursal = '';
 
-let GlobalEmpnit = 'DIST_JV';
-let GlobalEmpNombre = 'DISTRIBUIDORA';
+let GlobalEmpnit = 'LTJ-COBAN';
+let GlobalEmpNombre = 'LTJ COBAN';
 
 
 let GlobalVentaMinima = 79.99;
 
 let dataEmpresas = [ 
-    {codsucursal:"DIST_JV",nomsucursal:"DISTRIBUIDORA",color:"success"}
+    {codsucursal:"LTJ-COBAN",nomsucursal:"LTJ COBAN",color:"success"}
   ];
